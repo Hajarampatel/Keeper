9d2761bc58f3000e62602e46759895f4fc0080e8
HAJARAM <hajaram969@gmail.com>
2022-07-30 01:21:36 +0530

    first commit

---
+++ build/precache-manifest.fe9d4ae5b18b2e6e26d0f8a4422801ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ba14c98332bf153b70c78fe57c363f",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "b7e88276d4b88299a9d6",
+    "url": "/keeper/static/css/main.5c908c62.chunk.css"
+  },
+  {
+    "revision": "5056acffc7bad2e99fdf",
+    "url": "/keeper/static/js/2.86e029cc.chunk.js"
+  },
+  {
+    "revision": "b7e88276d4b88299a9d6",
+    "url": "/keeper/static/js/main.90181874.chunk.js"
+  },
+  {
+    "revision": "e8b20f9fd74d6529a86b",
+    "url": "/keeper/static/js/runtime-main.ff09c6ad.js"
+  }
+]);
